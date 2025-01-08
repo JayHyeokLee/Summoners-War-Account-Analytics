01f37c7270768f9031ab88989b8e52974aee4ac4
JayHyeokLee <123212773+JayHyeokLee@users.noreply.github.com>
2025-01-08 14:35:37 -0500

    set up the views skeleton
    still logic to implement

--- Backend/swjpBackend/upload/views.py
+++ Backend/swjpBackend/upload/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import json
+from rest_framework.decorators import api_view
+from rest_framework.decorators import Response
 
-# Create your views here.
+@api_view(['POST'])
+def upload_json(request):
+    #Getting upload file
+    file = request.FILES.get('file')
+
+    if not file:
+        return Response({'error': 'No file uploaded'}, status=400)
+    
+    try:
+        #loading and parsing json
+        data = json.load(file)
+
+        #need to insert processing logic here
+
+        return Response({'message': 'File uploaded successfully'})
+    except json.JSONDecodeError:
+        return Response({'error': 'Invalid JSON file'}, status=400)
+
