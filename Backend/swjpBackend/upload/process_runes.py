c13ad94e13667dab0570a113f91efce3d22f16f9
JayHyeokLee <123212773+JayHyeokLee@users.noreply.github.com>
2025-01-30 21:03:52 -0500

    debug

--- Backend/swjpBackend/upload/process_runes.py
+++ Backend/swjpBackend/upload/process_runes.py
@@ -22,21 +22,21 @@ def calculate_efficiency(rune):
     #if rune["class"] not in mapping["rune"]["effectTypes"]["main"][main_stat]["max"]:
     #    raise KeyError(f"Rune class {rune['class']} not found in max values")
     
-    max_main = mapping["rune"]["effectTypes"]["main"][main_stat]["max"][rune["class"]]
+    max_main = mapping["rune"]["mainstat"][main_stat]["max"][rune["class"]]
     ratio += main_value / max_main
 
     # Sub stat efficiency
     for stat in rune["sec_eff"]:
         stat_type = stat[0]
         stat_value = stat[1] + stat[3]  # Base value + bonus
-        max_sub = mapping["rune"]["effectTypes"]["sub"][stat_type]["max"][6]
+        max_sub = mapping["rune"]["substat"][stat_type]["max"][6]
         ratio += stat_value / max_sub
 
     # Prefix stat efficiency (innate stat)
     if rune.get("prefix_eff") and rune["prefix_eff"][0] > 0:
         prefix_stat = rune["prefix_eff"][0]
         prefix_value = rune["prefix_eff"][1]
-        max_prefix = mapping["rune"]["effectTypes"]["sub"][prefix_stat]["max"][6]
+        max_prefix = mapping["rune"]["substat"][prefix_stat]["max"][6]
         ratio += prefix_value / max_prefix
 
     # Efficiency calculations
