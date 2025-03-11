17862c15adf01391fcedceff12a541084965ca40
JayHyeokLee <123212773+JayHyeokLee@users.noreply.github.com>
2025-03-10 20:37:07 -0400

    fixed process_runes unit tests

---
+++ Backend/swjpBackend/upload/test_runes.py
@@ -0,0 +1,187 @@
+class TestRunes:
+    runes = [
+        {
+            "rune_id": 60739144488,
+            "wizard_id": 35900893,
+            "occupied_type": 2,
+            "occupied_id": 0,
+            "slot_no": 3,
+            "rank": 4,
+            "class": 6,
+            "set_id": 3,
+            "upgrade_limit": 15,
+            "upgrade_curr": 0,
+            "base_value": 237750,
+            "sell_value": 16173,
+            "pri_eff": [
+                5,
+                22
+            ],
+            "prefix_eff": [
+                0,
+                0
+            ],
+            "sec_eff": [
+                [
+                    2,
+                    8,
+                    0,
+                    0
+                ],
+                [
+                    6,
+                    7,
+                    0,
+                    0
+                ],
+                [
+                    1,
+                    224,
+                    0,
+                    0
+                ]
+            ],
+            "extra": 4
+        },
+        {
+            "rune_id": 54267878655,
+            "wizard_id": 35900893,
+            "occupied_type": 2,
+            "occupied_id": 0,
+            "slot_no": 3,
+            "rank": 5,
+            "class": 6,
+            "set_id": 2,
+            "upgrade_limit": 15,
+            "upgrade_curr": 12,
+            "base_value": 332850,
+            "sell_value": 58642,
+            "pri_eff": [
+                5,
+                118
+            ],
+            "prefix_eff": [
+                0,
+                0
+            ],
+            "sec_eff": [
+                [
+                    8,
+                    10,
+                    0,
+                    4
+                ],
+                [
+                    6,
+                    19,
+                    0,
+                    6
+                ],
+                [
+                    9,
+                    5,
+                    0,
+                    0
+                ],
+                [
+                    2,
+                    14,
+                    0,
+                    6
+                ]
+            ],
+            "extra": 5
+        },
+        {
+            "rune_id": 53664977198,
+            "wizard_id": 35900893,
+            "occupied_type": 2,
+            "occupied_id": 0,
+            "slot_no": 3,
+            "rank": 4,
+            "class": 6,
+            "set_id": 1,
+            "upgrade_limit": 15,
+            "upgrade_curr": 9,
+            "base_value": 237750,
+            "sell_value": 28975,
+            "pri_eff": [
+                5,
+                94
+            ],
+            "prefix_eff": [
+                10,
+                5
+            ],
+            "sec_eff": [
+                [
+                    2,
+                    13,
+                    0,
+                    0
+                ],
+                [
+                    9,
+                    11,
+                    0,
+                    0
+                ],
+                [
+                    8,
+                    11,
+                    0,
+                    0
+                ]
+            ],
+            "extra": 4
+        },
+        {
+            "rune_id": 40709721092,
+            "wizard_id": 35900893,
+            "occupied_type": 2,
+            "occupied_id": 0,
+            "slot_no": 4,
+            "rank": 5,
+            "class": 6,
+            "set_id": 1,
+            "upgrade_limit": 15,
+            "upgrade_curr": 15,
+            "base_value": 399420,
+            "sell_value": 129863,
+            "pri_eff": [
+                10,
+                80
+            ],
+            "prefix_eff": [
+                2,
+                8
+            ],
+            "sec_eff": [
+                [
+                    9,
+                    9,
+                    0,
+                    0
+                ],
+                [
+                    4,
+                    10,
+                    1,
+                    6
+                ],
+                [
+                    8,
+                    13,
+                    0,
+                    0
+                ],
+                [
+                    12,
+                    6,
+                    0,
+                    0
+                ]
+            ],
+            "extra": 4
+        },
+    ]
--- Backend/swjpBackend/upload/tests.py
+++ Backend/swjpBackend/upload/tests.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
-import test_runes as runes
-from process_runes import calculate_efficiency
+from upload.test_runes import TestRunes
+from upload.process_runes import calculate_efficiency
 
 class TestProcessRunes(TestCase):
     def test_calculate_efficiency(self):
-        output_runes = [calculate_efficiency(rune) for rune in runes]
+        output_runes = [calculate_efficiency(rune) for rune in TestRunes.runes]
         self.assertEqual(output_runes, [
             {"rune_id": 60739144488, "rune_set": 3, "current_efficiency": 27.69,},
             {"rune_id": 54267878655, "rune_set": 2, "current_efficiency": 98.47,},
