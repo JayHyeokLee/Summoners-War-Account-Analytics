44b0ee8ae37e6cf58e1744b2115bf23d7ca0eede
JayHyeokLee <123212773+JayHyeokLee@users.noreply.github.com>
2025-03-10 20:16:54 -0400

    added test cases for backend

---
+++ Backend/swjpBackend/upload/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+import test_runes as runes
+from process_runes import calculate_efficiency
+
+class TestProcessRunes(TestCase):
+    def test_calculate_efficiency(self):
+        output_runes = [calculate_efficiency(rune) for rune in runes]
+        self.assertEqual(output_runes, [
+            {"rune_id": 60739144488, "rune_set": 3, "current_efficiency": 27.69,},
+            {"rune_id": 54267878655, "rune_set": 2, "current_efficiency": 98.47,},
+            {"rune_id": 53664977198, "rune_set": 1, "current_efficiency": 61.27,},
+            {"rune_id": 40709721092, "rune_set": 1, "current_efficiency": 74.67,},])
