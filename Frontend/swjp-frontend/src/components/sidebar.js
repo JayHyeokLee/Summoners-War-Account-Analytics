8b1b1620279a20dcf4022ba84967acaf8ca59f22
JayHyeokLee <123212773+JayHyeokLee@users.noreply.github.com>
2025-03-18 19:36:59 -0400

    added background and gradient

--- Frontend/swjp-frontend/src/components/sidebar.js
+++ Frontend/swjp-frontend/src/components/sidebar.js
@@ -6,12 +6,12 @@ import "../bootstrap.css";
 export const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div class="container-l">
-        <nav class="navbar navbar-dark bg-dark rounded">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
           <a class="navbar-brand" href="#">
             <img src={logo} alt="Logo" className="sidebar-logo" width="80" height="40"/>
           </a>
-          <ul class="nav nav-pills">
+          <ul class="nav">
             <li class="nav-item active">
               <a class="nav-link" href="/">Rune Chart</a>
             </li>
